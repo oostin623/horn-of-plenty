50d2d0879f5c04564acb27c0296d88fc24246c66
oostin623 <oostin623@gmail.com>
2020-01-25 14:07:11 -0500

    initial development of food-listing, food-search, and create-food functionality for the food-database feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FoodDatabasePageComponent } from './food-database/food-database-page/food-database-page.component';
+import { FoodDatabaseSearchComponent } from './food-database/food-database-search/food-database-search.component';
+import { CreateFoodFormComponent } from './food-database/create-food-form/create-food-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodDatabasePageComponent,
+    FoodDatabaseSearchComponent,
+    CreateFoodFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
